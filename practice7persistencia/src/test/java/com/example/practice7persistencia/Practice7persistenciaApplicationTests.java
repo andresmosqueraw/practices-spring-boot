fc3d458eb587760677812fcdddcb015ae2c33894
cbazurto <andresmosquera739@gmail.com>
2024-08-27 14:59:51 -0500

    practica7

---
+++ practice7persistencia/src/test/java/com/example/practice7persistencia/Practice7persistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice7persistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice7persistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
